b562a598c31dd3f0e512b769a1312833bd824f99
pkorrie <patiencekorrie@gmail.com>
2020-03-13 20:12:51 -0400

    create enemy class

---
+++ src/enemy.js
@@ -0,0 +1,43 @@
+export class enemy{
+    constructor(position, speed, health, type){
+        this.position = position;
+        this.speed = speed;
+        this.health = health;
+        this.type = type;
+        this.dead = false;
+    }
+    getSpeed(){
+        return this.speed;
+    }
+    setSpeed(speed){
+        this.speed = speed;
+    }
+    getHealth(){
+        return this.health;
+    }
+    takeDamage(damage){
+        this.health = this.health - damage;
+        if(health<= 0){
+            this.die();
+        }
+    }
+    die(){
+        //do death things
+        this.speed = 0;
+        this.dead = true;
+    }
+    move(){
+
+    }
+    lose(){
+        this.speed = 0;
+    }
+    
+    win(){
+        this.speed = 0;
+    }
+    update(){
+        this.move;
+    }
+
+}
